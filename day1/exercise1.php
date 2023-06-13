ed8d6d8ff49d7b14c7dbd1b70f75298d66579b7c
Damien Deprez <1damiendeprez@gmail.com>
2023-06-13 09:07:28 +0200

    correction

---
+++ day1/exercise1.php
@@ -0,0 +1,20 @@
+<?php
+  $name = "Alex";
+  $age = 25;
+  
+  echo $name, $age;
+
+
+?>
+
+<?php
+  $name = "Halil";
+  $age = 27;
+  $job = "développeur";
+
+
+  echo "Je m'appelle" . $name, "j'ai" . $age "et je suis" . $job;
+?>
+
+
+
