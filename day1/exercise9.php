87cc4801ecc17584f457aa9f3788e0a431ede804
Damien Deprez <1damiendeprez@gmail.com>
2023-06-13 16:35:40 +0200

    excercise9 corrected

--- day1/exercise9.php
+++ day1/exercise9.php
@@ -1,11 +1,21 @@
 <?php 
    $numbers = array(21565, 3412, 180,
    1556, 182, 84, 15);
+   $number = array();
 
    foreach ($numbers as $value){
         if ($value % 3){
-            echo $value .(",");
+            array_push($number, $value);
+        } 
+    }
+    for ($i=0; $i<count($number); $i++){
+        if ($i>=1){
+            echo ", " . $number[$i];
         }
-   }
+        else {
+            echo $number[$i];
+        }
+        
+    }
 ?>
 
