81f3f6c13be7899bf4c40abed05be8ad7fb83fe2
Damien Deprez <1damiendeprez@gmail.com>
2023-06-13 11:05:18 +0200

    exercise5 done

---
+++ day1/exercise5.php
@@ -0,0 +1,18 @@
+<?php 
+    $str = "abcdefghijklmnopqrstuvwxyz";
+    $i = 0;
+
+    while ($i < strlen($str)) {
+        if ($i % 2){
+            echo $str[$i];
+        }
+        $i++;
+        
+    }
+?>
+
+
+
+
+
+
