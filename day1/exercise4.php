bbd58c46218207c2c6bc0d76dadb65a0c164c3e8
Damien Deprez <1damiendeprez@gmail.com>
2023-06-13 12:07:40 +0200

    exercise4 done

--- day1/exercise4.php
+++ day1/exercise4.php
@@ -1,11 +1,3 @@
-Écrivez une variable appelée « str » qui a pour valeur « Hello World ! ». Il en faudra également
-une nommée « i » qui sera égale à 0.
-Vous devez utiliser une boucle while() qui va vérifier que $i est strictement inférieur à la longueur de $str. Vous afficherez les caractères de $str un à un, séparés par un caractère espace, et incrémenterez $i de 1 à chaque fois.
-Attention : Faites attention à ne pas afficher le caractère qui marque la fin de la chaîne de caractères. Aussi, la fonction strlen() peut vous être utile pour calculer la longueur de $str.
-
-<br/>
-Exercise 4 :
-<br/>
 <?php
 $str = "Hello World !";
 $i = 0;
