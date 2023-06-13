f336fe63ca44a82300f573f4637497a3d67a2e28
Damien Deprez <1damiendeprez@gmail.com>
2023-06-13 12:07:08 +0200

    exercice1 done

--- day1/exercise1.php
+++ day1/exercise1.php
@@ -1,20 +1,6 @@
 <?php
-  $name = "Alex";
+$name = "Alex";
   $age = 25;
   
-  echo $name, $age;
-
-
-?>
-
-<?php
-  $name = "Halil";
-  $age = 27;
-  $job = "développeur";
-
-
-  echo "Je m'appelle" . $name, "j'ai" . $age "et je suis" . $job;
-?>
-
-
-
+  echo $name .$age;
+  ?>
